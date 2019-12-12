ce309afd4e67e543add0739de104cd16edfa7cb7
ekothe <pc.miss@gmail.com>
2019-12-12 13:23:02 +1100

    Cleaned up so it finds the data and loads libraries

--- R/find_by_site.R
+++ R/find_by_site.R
@@ -1,9 +1,16 @@
-library(readr)
+library(tidyverse)
+library(here)
+library(janitor)
 
-Sys.Date() - 1
+nswgov <- read_csv(here::here("data", "nswgov_daily.csv")) %>%
+  clean_names()
+
+nswgov_long <- nswgov %>%
+  select(1:27) %>%
+  pivot_longer(names_to = "site", values_to = "aqi", 2:27)
 
 my_site <- "sydney_central_east_raqi_24_hour_index"
 
 mysite_data <- nswgov_long %>%
-  filter(site == "sydney_central_east_raqi_24_hour_index")
+  filter(site == my_site)
 
