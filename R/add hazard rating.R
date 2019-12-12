d0d9aa323bd363d8f0ca29a44188300a72e9e9be
Jen Richmond <jennyrichmond@gmail.com>
2019-12-13 10:02:07 +1100

    adds hazard categories to percentile and writes to new csv

---
+++ R/add hazard rating.R
@@ -0,0 +1,21 @@
+library(tidyverse)
+library(here)
+
+
+#read percentile data data
+
+aqi <- read_csv(here("data", "aqi_percentile_joined.csv"))
+
+# add categories from epa
+# very good, moderate, poor, very poor, hazardous
+
+aqi_cat <- aqi %>%
+        mutate(category = case_when(between(aqi, 0, 33) ~ "Very Good", 
+                             between(aqi, 34, 66) ~ "Good", 
+                            between(aqi, 67, 99) ~ "Fair", 
+                            between(aqi, 100, 149) ~ "Foor",
+                            between(aqi, 150, 200) ~ "Very Poor",
+                            (aqi > 200 ~ "Hazardous")))
+
+aqi_cat %>% write_csv(here("data", "aqi_percentile_hazard_categories.csv"))     
+     
